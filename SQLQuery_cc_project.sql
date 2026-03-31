68aafc09c53ef77f0c38205d27e5b5d2b5cb61d6
Sidharth Mahapatra <sidharthm2611@gmail.com>
2026-03-31 13:16:28 +0530

    Add files via upload

---
+++ SQLQuery_cc_project.sql
@@ -0,0 +1,79 @@
+create database ccdb;
+
+-- 1. Create cc_detail table
+
+CREATE TABLE cc_detail (
+    Client_Num INT,
+    Card_Category VARCHAR(20),
+    Annual_Fees INT,
+    Activation_30_Days INT,
+    Customer_Acq_Cost INT,
+    Week_Start_Date DATE,
+    Week_Num VARCHAR(20),
+    Qtr VARCHAR(10),
+    current_year INT,
+    Credit_Limit DECIMAL(10,2),
+    Total_Revolving_Bal INT,
+    Total_Trans_Amt INT,
+    Total_Trans_Ct INT,
+    Avg_Utilization_Ratio DECIMAL(10,3),
+    Use_Chip VARCHAR(10),
+    Exp_Type VARCHAR(50),
+    Interest_Earned DECIMAL(10,3),
+    Delinquent_Acc VARCHAR(5)
+);
+
+-- 2. Create cc_detail table
+
+CREATE TABLE cust_detail (
+    Client_Num INT,
+    Customer_Age INT,
+    Gender VARCHAR(5),
+    Dependent_Count INT,
+    Education_Level VARCHAR(50),
+    Marital_Status VARCHAR(20),
+    State_cd VARCHAR(50),
+    Zipcode VARCHAR(20),
+    Car_Owner VARCHAR(5),
+    House_Owner VARCHAR(5),
+    Personal_Loan VARCHAR(5),
+    Contact VARCHAR(50),
+    Customer_Job VARCHAR(50),
+    Income INT,
+    Cust_Satisfaction_Score INT
+);
+
+-- Change the date column to VARCHAR temporarily
+ALTER TABLE dbo.cc_detail
+ALTER COLUMN Week_Start_Date VARCHAR(20);
+
+-- 3. Copy csv data into SQL (remember to update the file name and file location in below query)
+
+-- copy cc_detail table
+
+BULK INSERT cc_detail
+FROM 'D:\Project\Credit_card_project\cc_add.csv'
+WITH (
+    FIRSTROW        = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR   = '\n',
+    TABLOCK
+);
+
+select * from cc_detail;
+
+-- copy cust_detail table
+
+BULK INSERT cust_detail
+FROM 'D:\Project\Credit_card_project\cust_add.csv'
+WITH (
+    FIRSTROW        = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR   = '\n',
+    TABLOCK
+);
+
+select * from cust_detail;
+
+select * from cc_detail;
+select * from cust_detail;
